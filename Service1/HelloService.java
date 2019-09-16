9012a2c205110433c28efb684cdb7ccdfbf0e8e1
hackeroid080819 <53891847+hackeroid080819@users.noreply.github.com>
2019-09-16 10:00:32 +0300

    Add files via upload

---
+++ Service1/HelloService.java
@@ -0,0 +1,27 @@
+package com.example.itaykan.myapplication;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class HelloService extends Service {
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
