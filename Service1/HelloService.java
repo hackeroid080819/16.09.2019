8ef346c0af164f94a4dbe4429ccb452b96e8a2d0
hackeroid080819 <53891847+hackeroid080819@users.noreply.github.com>
2019-09-16 10:22:56 +0300

    Update HelloService.java

--- Service1/HelloService.java
+++ Service1/HelloService.java
@@ -3,6 +3,7 @@ package com.example.itaykan.myapplication;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.widget.Toast;
 
 public class HelloService extends Service {
     @Override
@@ -17,11 +18,18 @@ public class HelloService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        return super.onStartCommand(intent, flags, startId);
+        //return super.onStartCommand(intent, flags, startId);
+
+        Toast.makeText(this, "Service started!", Toast.LENGTH_SHORT).show();
+
+        return Service.START_STICKY;
     }
 
     @Override
     public void onDestroy() {
+        Toast.makeText(this, "Service destroyed!", Toast.LENGTH_SHORT).show();
+
         super.onDestroy();
+
     }
 }
