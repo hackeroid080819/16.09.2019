1fb9c4738431a0a9ff685627accc91512666d091
hackeroid080819 <53891847+hackeroid080819@users.noreply.github.com>
2019-09-16 10:22:03 +0300

    Update MainActivity.java

--- Service1/MainActivity.java
+++ Service1/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.itaykan.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.startBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent inte = new Intent(getBaseContext(), HelloService.class);
+                startService(inte);
+            }
+        });
+        findViewById(R.id.stopBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent inte = new Intent(getBaseContext(), HelloService.class);
+                stopService(inte);
+            }
+        });
     }
 }
